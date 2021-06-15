d48568631085fb3ff7480801afc59d51b3f8aabb
hegria <79516073+hegria@users.noreply.github.com>
2021-06-15 16:36:52 +0900

    거리 좀더 구체화 했음 실시간 추적 가능해짐!!!

--- app/src/main/java/com/example/ar_simple2/MainActivity.java
+++ app/src/main/java/com/example/ar_simple2/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     private float[][] position = new float[2][3];
     int touchth = 0;
     private TextView textView;
-    private List<Anchor> anchors = new ArrayList<Anchor>();
+    private List<AnchorNode> anchors = new ArrayList<AnchorNode>();
     private Button button;
 
     @Override
@@ -63,8 +63,8 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
                         builder.setMessage(throwable.getMessage()).show();
                         return null;
                     });
-            anchors.add(anchor);
-            anchor.getPose().getTranslation(position[touchth++],0);
+            //anchors.add(anchor);
+            anchor.getPose().getTranslation(position[touchth],0);
             arFragment.getArSceneView().getScene().addOnUpdateListener(this);
         }));
 
@@ -75,8 +75,8 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     public void onUpdate(FrameTime frameTime){
         Frame frame = arFragment.getArSceneView().getArFrame();
         if(touchth == 2){
-            anchors.get(0).getPose().getTranslation(position[0],0);
-            anchors.get(1).getPose().getTranslation(position[1],0);
+            anchors.get(0).getAnchor().getPose().getTranslation(position[0],0);
+            anchors.get(1).getAnchor().getPose().getTranslation(position[1],0);
 
             float distance = (float)Math.sqrt( (position[0][0]- position[1][0])
                     * (position[0][0]- position[1][0]) + (position[0][1]- position[1][1])*
@@ -85,7 +85,8 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
             textView.setText("Distance is "+Float.toString(distance));
         }
         if(touchth == 1){
-            anchors.get(0).getPose().getTranslation(position[0],0);
+            anchors.get(0).getAnchor().getPose().getTranslation(position[0],0);
+            //anchors.get(0).getPose().getTranslation(position[0],0);
             Pose cameraPose = frame.getCamera().getPose();
             float dx = position[0][0] - cameraPose.tx();
             float dy = position[0][1] - cameraPose.ty();
@@ -101,10 +102,13 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
 
     private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
         AnchorNode anchorNode = new AnchorNode(anchor);
+        anchors.add(anchorNode);
+        touchth++;
         TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
         transformableNode.setParent(anchorNode);
         transformableNode.setRenderable(modelRenderable);
         arFragment.getArSceneView().getScene().addChild(anchorNode);
+
         transformableNode.setOnTapListener(((hitTestResult, motionEvent) -> {
             Node node = hitTestResult.getNode();
             button.setOnClickListener(new View.OnClickListener() {
