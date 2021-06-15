3a1bcc9fdc93e8b8e40934fcbdb7a38f6fba362a
hegria <79516073+hegria@users.noreply.github.com>
2021-06-15 11:53:29 +0900

    object remove 기능 추가, update 호출 시 거리를 다시 산출하는 기능 구현(좀더 구체화해봐야할듯..)

--- app/src/main/java/com/example/ar_simple2/MainActivity.java
+++ app/src/main/java/com/example/ar_simple2/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,16 +17,22 @@ import com.google.ar.core.Pose;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements Scene.OnUpdateListener {
     private ArFragment arFragment;
     private float[][] position = new float[2][3];
     int touchth = 0;
     private TextView textView;
+    private List<Anchor> anchors = new ArrayList<Anchor>();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
         textView = (TextView) findViewById(R.id.textView);
-
+        button = (Button) findViewById(R.id.removebutton);
 
         arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
             if(touchth == 2){
@@ -55,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
                         builder.setMessage(throwable.getMessage()).show();
                         return null;
                     });
-
+            anchors.add(anchor);
             anchor.getPose().getTranslation(position[touchth++],0);
             arFragment.getArSceneView().getScene().addOnUpdateListener(this);
         }));
@@ -67,6 +75,9 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
     public void onUpdate(FrameTime frameTime){
         Frame frame = arFragment.getArSceneView().getArFrame();
         if(touchth == 2){
+            anchors.get(0).getPose().getTranslation(position[0],0);
+            anchors.get(1).getPose().getTranslation(position[1],0);
+
             float distance = (float)Math.sqrt( (position[0][0]- position[1][0])
                     * (position[0][0]- position[1][0]) + (position[0][1]- position[1][1])*
                     (position[0][1]- position[1][1])+(position[0][2]- position[1][2])*
@@ -74,6 +85,7 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
             textView.setText("Distance is "+Float.toString(distance));
         }
         if(touchth == 1){
+            anchors.get(0).getPose().getTranslation(position[0],0);
             Pose cameraPose = frame.getCamera().getPose();
             float dx = position[0][0] - cameraPose.tx();
             float dy = position[0][1] - cameraPose.ty();
@@ -81,6 +93,9 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
             float distance = (float)Math.sqrt(dx*dx+dy*dy+dz*dz);
 
             textView.setText("One obejct is chousen. The distance of Camera is " + Float.toString(distance));
+        }
+        if(touchth == 0){
+            textView.setText("Obejct is not choosen");
         }
     }
 
@@ -90,6 +105,19 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
         transformableNode.setParent(anchorNode);
         transformableNode.setRenderable(modelRenderable);
         arFragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.setOnTapListener(((hitTestResult, motionEvent) -> {
+            Node node = hitTestResult.getNode();
+            button.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                          public void onClick(View v) {
+                                              anchorNode.removeChild(transformableNode);
+                                              arFragment.getArSceneView().getScene().onRemoveChild(anchorNode);
+                                              anchors.remove(anchor);
+                                              touchth--;
+                                          }
+                                      }
+
+            );}));
         transformableNode.select();
 
     }
